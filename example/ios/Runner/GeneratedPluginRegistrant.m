f76f26b6572d175fc5da0598f4c2a67308db4a37
Abhishek Pal <abhishekpal2006@gmail.com>
2018-06-01 11:21:19 -0400

    Add files via upload

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <flutter_toast/FlutterToastPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterToastPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterToastPlugin"]];
+}
+
+@end
